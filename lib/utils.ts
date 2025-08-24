c1e8589c51634bfcb9bd89f4bc48c485ec74d908
“Stewar”t <stewarteaton05@gmail.com>
2025-08-24 11:39:41 -0400

    color Map in utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,18 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const taskColors = new Map([
+  ["red", "#ef4444"],
+  ["orange", "#f97316"],
+  ["yellow", "#eab308"],
+  ["green", "#22c55e"],
+  ["blue", "#3b82f6"],
+  ["indigo", "#6366f1"],
+  ["purple", "#8b5cf6"],
+  ["pink", "#ec4899"],
+  ["gray", "#78716c"],
+]);
