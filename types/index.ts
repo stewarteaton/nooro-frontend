7c2e866cd735da811f016b45650499477d326f4b
“Stewar”t <stewarteaton05@gmail.com>
2025-08-24 11:39:27 -0400

    global types

---
+++ types/index.ts
@@ -0,0 +1,19 @@
+export enum ViewEnum {
+  LIST = "list",
+  CREATE = "create",
+  EDIT = "edit",
+}
+
+export interface Task {
+  id: string;
+  text: string;
+  completed: boolean;
+  createdAt: Date;
+  color: string;
+}
+
+export interface TaskData {
+  text: string;
+  completed: boolean;
+  color: string;
+}
