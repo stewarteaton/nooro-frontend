41d700785da6509461004d83c6eebdb0955d1130
“Stewar”t <stewarteaton05@gmail.com>
2025-08-24 15:35:06 -0400

    update fallback url for api_base_url

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000";
 
 export const api = {
   // Tasks
